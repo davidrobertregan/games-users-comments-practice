9f3c679a4a9e43a581c97a5d9edc962b0bf08b5b
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2022-01-12 13:42:14 -0700

    comments#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,4 +22,23 @@ class CommentsController < ApplicationController
             render json: { error: "Comment not found"}, status: :not_found
         end
     end
+
+    def update 
+        comment = Comment.find_by(id: params[:id])
+        if comment
+            comment.update(comment_params)
+            render json: comment, status: :ok
+        else
+            render json: { error: "Comment not found"}, status: :not_found
+        end
+    end
+
+    def create
+        comment = current_user.comments.create(comment_params)
+        render json: comment, status: :created
+    end
+
+    def comment_params
+        params.permit(:content, :game_id)
+    end
 end
