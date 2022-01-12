bd5a1223ccd7492502722cbe7d93f3e977bdb0c3
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2022-01-12 10:42:07 -0700

    model associations

---
+++ db/migrate/20220112173202_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comments do |t|
+      t.string :content
+      t.belongs_to :user
+      t.belongs_to :game
+
+      t.timestamps
+    end
+  end
+end
