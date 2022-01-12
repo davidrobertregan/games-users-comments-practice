ea84e42da01e4e09057109d480c8093cd98b2af2
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2022-01-12 11:05:38 -0700

    seeds.rb with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+Game.destroy_all
+Comment.destroy_all
+
+puts "creating users"
+users = User.create([{name: "David"}, {name: "Ian"}, {name: "JP"}])
+
+puts "creating games"
+10.times do |i|
+    Game.create(name: Faker::Game.title, genre: Faker::Game.genre)
+end
+
+puts "creating comments"
+50.times do |i| 
+    Comment.create(content: Faker::Quote.most_interesting_man_in_the_world, game: Game.all.sample, user: User.all.sample)
+end
