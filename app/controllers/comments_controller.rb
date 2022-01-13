558142e736b6a3c13efea31e58bebf1eca252e4b
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2022-01-13 09:21:57 -0700

    find_comment refactor in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,44 +1,35 @@
 class CommentsController < ApplicationController 
-
+    before_action :find_comment, except: [:index, :create]
+    
     def index 
         render json: Comment.all
     end
 
     def show 
-        comment = Comment.find_by(id: params[:id])
-        if comment
-            render json: comment
-        else
-            render json: {error: "Comment not found"}, status: :not_found
-        end
+        render json: @comment
     end
 
     def destroy
-        comment = Comment.find_by(id: params[:id])
-        if comment
-            comment.destroy
-            head :no_content
-        else
-            render json: { error: "Comment not found"}, status: :not_found
-        end
+        @comment.destroy
+        head :no_content
     end
 
     def update 
-        comment = Comment.find_by(id: params[:id])
-        if comment
-            comment.update(comment_params)
-            render json: comment, status: :ok
-        else
-            render json: { error: "Comment not found"}, status: :not_found
-        end
+        @comment.update(comment_params)
+        render json: @comment, status: :ok
     end
 
     def create
-        comment = current_user.comments.create(comment_params)
+        comment = Comment.create(comment_params)
         render json: comment, status: :created
     end
 
     def comment_params
-        params.permit(:content, :game_id)
+        params.permit(:content, :game_id, :user_id)
+    end
+
+    def find_comment
+        @comment = Comment.find_by(id: params[:id])
+        render json: { error: "Comment not found"}, status: :not_found unless @comment
     end
 end
